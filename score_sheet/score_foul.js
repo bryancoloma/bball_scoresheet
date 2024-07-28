028e46452769448503a251c44f1a22cf410fbe72
bryancoloma <brycoloma@gmail.com>
2024-07-28 09:44:01 -0700

    added away point update js

--- score_sheet/score_foul.js
+++ score_sheet/score_foul.js
@@ -52,7 +52,7 @@ function decrementHomeTotalFouls(subHomeFoulsId, homeFoulDecrement) {
     addTotalHomeFouls();
 }
 
-//Calculate Total Fouls
+//Calculate Total Home Fouls
 function addTotalHomeFouls() {
     const addAllHomeFouls = document.querySelectorAll('h3[id^="addHomeTotalFouls"]');
     let homeTotalFouls = 0;
@@ -62,3 +62,33 @@ function addTotalHomeFouls() {
     document.getElementById('homeTotalFouls').value = homeTotalFouls;
 }
 
+
+// --------------------- Away
+
+//Add away points
+function incrementAwayTotalPoints(addAwayPointsId, awayPointIncrement) {
+    const totalAwayPointElementAdd = document.getElementById(addAwayPointsId);
+    let currentAwayPointAdd = parseInt(totalAwayPointElementAdd.textContent, 10);
+    currentAwayPointAdd += awayPointIncrement;
+    totalAwayPointElementAdd.textContent = currentAwayPointAdd;
+    addTotalAwayPoints();
+}
+
+//Subtract away points
+function decrementAwayTotalPoints(subAwayPointsId, awayPointDecrement) {
+    const totalAwayPointElementSub = document.getElementById(subAwayPointsId);
+    let currentAwayPointSub = parseInt(totalAwayPointElementSub.textContent, 10);
+    currentAwayPointSub -= awayPointDecrement;
+    totalAwayPointElementSub.textContent = currentAwayPointSub;
+    addTotalAwayPoints();
+}
+
+//Calculate total away points
+function addTotalAwayPoints() {
+    const addAllAwayPoints = document.querySelectorAll('h3[id^="addAwayTotalPoints"]');
+        let awayTotalPoints = 0;
+    addAllAwayPoints.forEach(awayPoints => {
+        awayTotalPoints += parseInt(awayPoints.textContent, 10);
+    })
+    document.getElementById('awayTotalScore').value = awayTotalPoints;
+}
